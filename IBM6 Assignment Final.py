94c6823be466dac43d41518b2fdd7f7943f1153b
iZotop79 <114948411+iZotop79@users.noreply.github.com>
2023-02-08 11:17:36 +0100

    Add files via upload

---
+++ IBM6 Assignment Final.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 09:30:32 2023
+
+@author: ivanz
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 19:42:20 2023
+
+@author: ivanz
+"""
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+pio.renderers.default='browser'
+
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+#Q1
+tesla = yf.Ticker("TSLA")   
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
+
+#Q2
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data  = requests.get(url).text
+
+
+soup = BeautifulSoup(html_data, 'html5lib')
+tesla_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+
+for table in soup.find_all('table'):
+
+    if ('Tesla Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        
+        for row in rows:
+            col = row.find_all('td')
+            
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',','').replace('$','')
+
+                tesla_revenue = tesla_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+tesla_revenue.tail()
+
+
+#Q3
+
+gamestop = yf.Ticker("GME")  
+gme_data = gamestop.history(period="max")
+gme_data.reset_index(inplace=True)
+gme_data.head()
+
+
+#Q4
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+html_data  = requests.get(url).text
+soup = BeautifulSoup(html_data, 'html5lib')
+
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for table in soup.find_all('table'):
+
+    if ('GameStop Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        
+        for row in rows:
+            col = row.find_all('td')
+            
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',','').replace('$','')
+
+                gme_revenue = gme_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+
+gme_revenue.tail()
+
+#Q5
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+
+#Q6
+make_graph(gme_data, gme_revenue, 'GameStop')
